85edac82af3ad2ec58b5d23c0ab172453cfb26e1
area1110 <khanhhuy1110@gmail.com>
2021-07-29 18:13:47 +0700

    yes

--- script/script.js
+++ script/script.js
@@ -1,56 +1,147 @@
 var main = document.getElementById("mainView");
 var ctx = main.getContext("2d");
 var pix = ctx.create;
-var width = 25,
-  heigth = 25;
-var x = 0,
-  y = 0;
+var width = main.width / 36,
+  height = main.height / 36;
+var x = main.width / 2 - width,
+  y = main.height - 2 * height;
 
+//---Moving Function---
 var move = 25;
-
-ctx.beginPath();
-ctx.rect(x, y, width, heigth);
-ctx.fill();
-
 function right() {
-  if (x != main.width-25) {
-    x += move;
-  }
- 
+  if (x < main.width - 3 * width) x += move;
 }
 
 function left() {
-  if (x != 0) x -= move;
-  //   ctx.clearRect(0, 0, main.width, main.height);
+  if (x > 0) x -= move;
 }
 
 function down() {
-  if (y != main.height - 25) {
+  if (y < main.height - 2 * height) {
     y += move;
   }
-  //   ctx.clearRect(0, 0, main.width, main.height);
 }
 
 function up() {
-  if (y != 0) {
+  if (y > 0) {
     y -= move;
   }
-  //   ctx.clearRect(0, 0, main.width, main.height);
+}
+//-----------------------
+
+//---Transform
+var head = new Object();
+head.x = x + width;
+head.y = y;
+function setHead(x, y) {
+  head.x = x;
+  head.y = y;
 }
 
-function shoot() {}
-
-function drawMainChar() {
+function drawMainCharUp() {
   ctx.beginPath();
-  ctx.rect(x, y, width, heigth);
+  ctx.rect(x + width, y, width, height);
+  ctx.fill();
+  ctx.rect(x, y + height, width, height);
+  ctx.fill();
+  ctx.rect(x + width, y + height, width, height);
+  ctx.fill();
+  ctx.rect(x + 2 * width, y + height, width, height);
   ctx.fill();
   ctx.closePath();
 }
 
-function draw() {
-    ctx.clearRect(0, 0, main.width, main.height);
-    drawMainChar();
+function drawMainCharDown() {
+  ctx.beginPath();
+  ctx.rect(x + width, y + 2 * height, width, height);
+  ctx.fill();
+  ctx.rect(x, y + height, width, height);
+  ctx.fill();
+  ctx.rect(x + width, y + height, width, height);
+  ctx.fill();
+  ctx.rect(x + 2 * width, y + height, width, height);
+  ctx.fill();
+  ctx.closePath();
 }
+
+function drawMainCharLeft() {
+  ctx.beginPath();
+  ctx.rect(x + width, y, width, height);
+  ctx.fill();
+  ctx.rect(x, y + height, width, height);
+  ctx.fill();
+  ctx.rect(x + width, y + height, width, height);
+  ctx.fill();
+  ctx.rect(x + width, y + 2 * height, width, height);
+  ctx.fill();
+  ctx.closePath();
+}
+function drawMainCharRight() {
+  ctx.beginPath();
+  ctx.rect(x + width, y, width, height);
+  ctx.fill();
+  ctx.rect(x + 2 * width, y + height, width, height);
+  ctx.fill();
+  ctx.rect(x + width, y + height, width, height);
+  ctx.fill();
+  ctx.rect(x + width, y + 2 * height, width, height);
+  ctx.fill();
+  ctx.closePath();
+}
+
+var statusMain = "u";
+function drawMainChar() {
+  switch (statusMain) {
+    case "u":
+      drawMainCharUp();
+      setHead(x + width, y);
+      break;
+    case "d":
+      drawMainCharDown();
+      setHead(x + width, y + 2 * height);
+      break;
+    case "l":
+      drawMainCharLeft();
+      setHead(x, y + height);
+      break;
+    case "r":
+      drawMainCharRight();
+      setHead(x + 2 * width, y + height);
+      break;
+    default:
+      break;
+  }
+}
+
+//-------------------------------------------------
+
+var wB = width - 10,
+  hB = height - 10;
+var bullLoc = new Object();
+(bullLoc.xB = head.x), (bullLoc.yB = head.y);
+
+function drawBullet() {
+  ctx.beginPath();
+  ctx.rect(xB, yB, wB, hB);
+  ctx.fill();
+  ctx.closePath();
+}
+
+function shoot(){
+  
+}
+
+function draw() {
+  ctx.clearRect(0, 0, main.width, main.height);
+  drawMainChar();
+  drawBullet();
+}
+
+// function shoot(){
+//   switch(){
+
+//   }
+// }
 
 window.addEventListener(
   "keydown",
@@ -58,24 +149,31 @@ window.addEventListener(
     if (event.defaultPrevented) {
       return; // Do nothing if the event was already processed
     }
+    var countKey;
     switch (event.key) {
       case "ArrowDown":
         // code for "down arrow" key press.
-        down();
+
+        if (statusMain != "d") statusMain = "d";
+        else down();
         break;
       case "ArrowUp":
         // code for "up arrow" key press.
-        up();
+        if (statusMain != "u") statusMain = "u";
+        else up();
         break;
       case "ArrowLeft":
         // code for "left arrow" key press.
-        left();
+        if (statusMain != "l") statusMain = "l";
+        else left();
         break;
       case "ArrowRight":
         // code for "right arrow" key press.
-        right();
+        if (statusMain != "r") statusMain = "r";
+        else right();
         break;
       case " ":
+        shoot();
         break;
       default:
         return; // Quit when this doesn't handle the key event.
